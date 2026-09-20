458c097c7415efe977f8ac010dd37691c57b19b9
HUNT <121392683+TH3HUNT3Y@users.noreply.github.com>
2026-09-20 22:03:40 +0000

    Switch GUI to native Win32 no-OpenGL build

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,6 @@ mod input;
 mod text;
 mod typing;
 
-fn main() -> eframe::Result {
-    eframe::run_native(
-        "Auto Write",
-        eframe::NativeOptions {
-            viewport: eframe::egui::ViewportBuilder::default()
-                .with_inner_size([1240.0, 780.0])
-                .with_min_inner_size([920.0, 620.0]),
-            ..Default::default()
-        },
-        Box::new(|cc| Ok(Box::new(app::AutoWriteApp::new(cc)))),
-    )
+fn main() {
+    app::run();
 }
